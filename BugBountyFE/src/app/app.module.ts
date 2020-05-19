1de16cb30fb7d246f631a18407415298ba9da957
magana.armando52 <magana.armando52@gmail.com>
2020-05-18 22:48:16 -0500

    created rank display for current logged in user

--- BugBountyFE/src/app/app.module.ts
+++ BugBountyFE/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserRankComponent } from './components/user-rank/user-rank.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    UserRankComponent
   ],
   imports: [
     BrowserModule,
--- BugBountyFE/src/app/services/user.service.ts
+++ BugBountyFE/src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private userUrl:String = 'http://localhost:8080/user';
+
+  constructor(private http:HttpClient) { }
+
+  getRank(username: String) : Observable<any>{
+    return this.http.get<any>(`${this.userUrl}/userrank/${username}`);
+  
+  }
 }
