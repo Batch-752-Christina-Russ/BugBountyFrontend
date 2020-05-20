caec726273dabe413eccb6b7f8a21f0e2165b4b4
Drwildy <drwildeoffical@gmail.com>
2020-05-19 19:38:15 -0500

    Fixed cards and modals

--- BugBountyFE/src/app/components/approve-deny-bug/approve-deny-bug.component.ts
+++ BugBountyFE/src/app/components/approve-deny-bug/approve-deny-bug.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import { BugReport } from 'src/app/models/BugReport';
 import { BugReportService } from 'src/app/services/bug-report.service';
 import { Role } from 'src/app/models/Role';
@@ -17,33 +17,36 @@ bugReports:BugReport[];
   constructor(private brs:BugReportService) { }
 
   ngOnInit(): void {
-    this.bugReports=[new BugReport(1, null, null, 'BugBounty', null, 
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'High', null, 'pending'),
-    new BugReport(2, null, null, 'BugBounty', null, 
-    'Lorem ipsum. ',
-    'Lorem ipsum dolor.',
-    'High', null, 'pending'),
-    new BugReport(3, null, null, 'BugBounty', null, 
-    'Lorem ipsum. ',
-    'Lorem ipsum dolor.',
-    'High', null, 'pending'),
-    new BugReport(4, null, null, 'BugBounty', null, 
-    'Woof Woof ',
-    'Lorem ipsum dolor.',
-    'High', null, 'pending'),
-    new BugReport(5, null, null, 'BugBounty', null, 
-    'AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH',
-    'Lorem ipsum dolor.',
-    'High', null, 'pending'),
-    new BugReport(6, null, null, 'BugBounty', null, 
-    'It is done ',
-    'Lorem ipsum dolor.',
-    'High', null, 'pending')]
-    // this.brs.getPendingBugReports().subscribe(bugReports => {
-    //   this.bugReports = bugReports;
-    // })
+    // this.bugReports=[new BugReport(1, null, null, 'BugBounty', null, 
+    // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+    // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // 'High', null, 'pending'),
+    // new BugReport(2, null, null, 'BugBounty', null, 
+    // 'Lorem ipsum. ',
+    // 'Lorem ipsum dolor.',
+    // 'High', null, 'pending'),
+    // new BugReport(3, null, null, 'BugBounty', null, 
+    // 'Lorem ipsum. ',
+    // 'Lorem ipsum dolor.',
+    // 'High', null, 'pending'),
+    // new BugReport(4, null, null, 'BugBounty', null, 
+    // 'Woof Woof ',
+    // 'Lorem ipsum dolor.',
+    // 'High', null, 'pending'),
+    // new BugReport(5, null, null, 'BugBounty', null, 
+    // 'AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH',
+    // 'Lorem ipsum dolor.',
+    // 'High', null, 'pending'),
+    // new BugReport(6, null, null, 'BugBounty', null, 
+    // 'It is done ',
+    // 'Lorem ipsum dolor.',
+    // 'High', null, 'pending')]
+    this.brs.getPendingBugReports().subscribe(bugReports => {
+      this.bugReports = bugReports;
+    })
   }
 
+  deleteCard(event:any, i:any){
+
+  }
 }
