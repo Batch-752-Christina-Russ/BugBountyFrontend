0b6462f13849a4229cfcdcffdfe34b0f42de9297
Drwildy <Drwildeoffical@gmail.com>
2020-05-20 17:22:45 -0500

    Update: delete test
    Comment of non-working test deleted

--- BugBountyFE/src/app/app.component.spec.ts
+++ BugBountyFE/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('BugBountyFE');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('BugBountyFE app is running!');
-  // });
 });
