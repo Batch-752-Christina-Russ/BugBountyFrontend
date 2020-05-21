efb1c5021205061827231c14c9fa7017db761412
magana.armando52 <magana.armando52@gmail.com>
2020-05-21 11:26:38 -0500

    added routes for the resolve and leaderboard components

--- BugBountyFE/src/app/app-routing.module.ts
+++ BugBountyFE/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ApproveDenyBugComponent } from './components/approve-deny-bug/approve-deny-bug.component';
+import { ResolveComponent } from './components/resolve/resolve.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo:'index', pathMatch: 'full' },
   { path: 'home', component:HomeComponent },
   { path: 'index', component:LoginComponent },
-  { path: 'pendingbug', component:ApproveDenyBugComponent }
+  { path: 'pendingbug', component:ApproveDenyBugComponent },
+  { path: 'resolvebug', component:ResolveComponent },
+  { path: 'leaderboard', component:LeaderboardComponent}
 ];
 
 @NgModule({
