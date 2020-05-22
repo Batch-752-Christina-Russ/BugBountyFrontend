fc232743f5cb688e976786e90592995bd5116657
magana.armando52 <magana.armando52@gmail.com>
2020-05-22 09:48:28 -0500

    added user rank for the leaderboard css changes to resolve page

--- BugBountyFE/src/app/components/open-bugs/open-bugs.component.ts
+++ BugBountyFE/src/app/components/open-bugs/open-bugs.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { OpenBugsService } from '../../services/openbugs.service';
 import { Role } from 'src/app/models/Role';
 import { User } from 'src/app/models/User';
-import {DatePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -14,9 +14,9 @@ import {DatePipe } from '@angular/common';
 })
 export class OpenBugsComponent implements OnInit {
 
-  constructor(private bugService : OpenBugsService, private datePipe: DatePipe) { }
+  constructor(private bugService: OpenBugsService, private datePipe: DatePipe) { }
 
-  
+
 
   ngOnInit(): void {
 
@@ -24,34 +24,34 @@ export class OpenBugsComponent implements OnInit {
 
     // this.inputBugReport();
   }
-  
-  
-  BugReports : BugReport[];
 
- //Dummy data!!
-  inputBugReport(){
-  let roles1:Role = new Role(1, "user");
-  let roles2:Role = new Role(2, "admin");
-  let user1:User = new User(0,"user1", "p1", 0, roles1);
-  let user2:User = new User(1, "user2", "p2", 0, roles2);
-  this.BugReports = [new BugReport(0, user1, user2, "asdf", "asdf", "asf", "asd", "asdf", new Date(), "open")];
+
+  BugReports: BugReport[];
+
+  //Dummy data!!
+  inputBugReport() {
+    let roles1: Role = new Role(1, "user");
+    let roles2: Role = new Role(2, "admin");
+    let user1: User = new User(0, "user1", "p1", 0, roles1);
+    let user2: User = new User(1, "user2", "p2", 0, roles2);
+    this.BugReports = [new BugReport(0, user1, user2, "asdf", "asdf", "asf", "asd", "asdf", new Date(), "open")];
   }
 
-  
-    viewOpenBugs(){
-      this.bugService.viewOpenBugs().subscribe(
-        (data) =>{
-          console.log(data);
-          console.log(data[0]);
-          this.BugReports = data; 
-        },
-        () => {
-          console.log("There was an error grabbing your bug reports!");
-        }
-      )
 
-    }
+  viewOpenBugs() {
+    this.bugService.viewOpenBugs().subscribe(
+      (data) => {
+        console.log(data);
+        console.log(data[0]);
+        this.BugReports = data;
+      },
+      () => {
+        console.log("There was an error grabbing your bug reports!");
+      }
+    )
 
-  
   }
+
+
+}
 
